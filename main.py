441293c3fc988a880a3a38674bfd8c84f262f66f
mahsa-taer <fatemeh.taer@gmail.com>
2020-02-25 15:39:57 -0800

    New Version

--- main.py
+++ main.py
@@ -12,14 +12,6 @@ if __name__ == "__main__":
     patient2 = Patient("Bill", "Stark", "1960-9-2", "1111 Columbia, New Westminster, BC", 589, 54600, 2)
     patient3 = Patient("Jame", "O'Conner", "1966-8-1", "433 Bigbang, New Westminster, BC", 610, 7500, 3)
 
-    # try:
-    #     doctor1.set_id(13)
-    #     # doctor2.set_id(15)
-    #     # patient1.set_id(100009)
-    #     # patient2.set_id(100010)
-    #     # patient3.set_id(100011)
-    # except TypeError:
-    #     print("Id should be an integer.")
     print(doctor1.get_description())
     print(doctor1.get_type())
     print(doctor2.get_description())
