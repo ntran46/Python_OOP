f1a1556d64ab4909a283a7ab0a346a250dd1bf21
ntran46 <ntran46@my.bcit.ca>
2020-02-25 16:14:06 -0800

    Update function get_total_bill_amount_released_patients Put doctrings for test_accounting_stats

--- accounting_stats.py
+++ accounting_stats.py
@@ -1,29 +1,33 @@
 
 """This is the AccountingStats class which shows the statistics of the accounting department of the hospital"""
+
+
 class AccountingStats:
 
-    """"""
+    """Define the AccountingStats class"""
+
     def __init__(self, released_patient: int, not_released_patient: int, total_bill):
-        """"""
+        """Initialize a constructor of a AccountingStats object"""
         self.validation(released_patient, not_released_patient, total_bill)
         self._released_patient_num = released_patient
         self._not_released_patient_num = not_released_patient
-        self._total_bill_amount_each_person = total_bill
+        self._total_bill_amount_released_patients = total_bill
 
     def get_released_patient_num(self):
-        """"""
+        """Function to get the total number of patient that is being treated"""
         return self._released_patient_num
 
     def get_not_released_patient_num(self):
-        """"""
+        """Function to get the total number of patient that is covered"""
         return self._not_released_patient_num
 
-    def get_total_bill_amount_each_person(self):
-        """"""
-        return self._total_bill_amount_each_person
+    def get_total_bill_amount_released_patients(self):
+        """Function to get the total bill amount of all released patient"""
+        return self._total_bill_amount_released_patients
 
     @classmethod
     def validation(cls, released_patient: int, not_released_patient: int, total_bill: int):
+        """Function to validate all attributes"""
         if released_patient < 1 or type(released_patient) is not int:
             raise ValueError("Released patient numbers cannot be 0. It should be "
                              "more than 0. Released patient numbers should be an "
--- test/test_accounting_stats.py
+++ test/test_accounting_stats.py
@@ -1,14 +1,22 @@
 from unittest import TestCase
 from accounting_stats import AccountingStats
 
+
 class TestAccountingStats(TestCase):
+
+    """This is a test class to test the AccountingStats class"""
+
     def setUp(self) -> None:
+        """Initialize setup object"""
         self.stats = AccountingStats(5, 100, 150000)
 
-    def test_constructor(self):
+    def test_valid_constructor(self):
+        """Test an object is created correctly"""
         self.assertIsNotNone(self.stats)
         self.assertIsInstance(self.stats, AccountingStats)
 
+    def test_invalid_constructor(self):
+        """Test an object with invalid parameters"""
         with self.assertRaises(TypeError):
             stats_1 = AccountingStats("5", 100, 150000)
 
@@ -22,19 +30,22 @@ class TestAccountingStats(TestCase):
             stats_4 = AccountingStats(5, 100, 200000)
 
         with self.assertRaises(ValueError):
-            stats_4 = AccountingStats(0, 100, 150000)
+            stats_5 = AccountingStats(0, 100, 150000)
 
         with self.assertRaises(ValueError):
-            stats_4 = AccountingStats(5, 0, 150000)
+            stats_6 = AccountingStats(5, 0, 150000)
 
     def test_get_released_patient_num(self):
+        """Test to get the total number of released patients"""
         self.assertEqual(self.stats.get_released_patient_num(), 5)
         self.assertIsNotNone(self.stats.get_released_patient_num())
 
     def test_get_not_released_patient_num(self):
+        """Test to get the total number of current patients"""
         self.assertEqual(self.stats.get_not_released_patient_num(), 100)
         self.assertIsNotNone(self.stats.get_not_released_patient_num())
 
-    def test_get_total_bill_amount_each_person(self):
-        self.assertEqual(self.stats.get_total_bill_amount_each_person(), 150000)
-        self.assertIsNotNone(self.stats.get_total_bill_amount_each_person())
+    def test_get_total_bill_amount_released_patients(self):
+        """Test to get the total amount of all released patients"""
+        self.assertEqual(self.stats.get_total_bill_amount_released_patients(), 150000)
+        self.assertIsNotNone(self.stats.get_total_bill_amount_released_patients())
