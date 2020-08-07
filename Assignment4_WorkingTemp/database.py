a1a0d9ecbd28dc6d9d0ec0a54a28156e7f6ea724
Uy Tran <ntran46@my.bcit.ca>
2020-08-06 17:19:56 -0700

    Remove temp files

--- Assignment4_WorkingTemp/database.py
+++
@@ -1,4 +0,0 @@
-from peewee import SqliteDatabase
-
-db = SqliteDatabase("person.db", pragmas={'ignore_check_constraints': 0})
-db.connect()
--- Assignment4_WorkingTemp/drop_tables.py
+++
@@ -1,8 +0,0 @@
-from database import db
-from Model.doctor import Doctor
-from Model.department import Department
-from Model.patient import Patient
-
-"""This is to connect to database and drop tables"""
-
-db.drop_tables([Department, Doctor, Patient])
